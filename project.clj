aac41bf9a747c720fd2fc98d3b327b79b3bfdbf6
Alex ter Weele <alex.ter.weele@gmail.com>
2019-11-02 12:38:34 -0500

    Begin writing README.org, make Maven coordinate.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject extensible-cond "0.1.0-SNAPSHOT"
+(defproject name.atw/extensible-cond "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
